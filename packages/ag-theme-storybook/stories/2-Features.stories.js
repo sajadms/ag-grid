c4821f1b6f29855ebdab9aefb8b143e0d8b66450
Petyo Ivanov <underlog@gmail.com>
2019-09-18 21:05:23 +0300

    Column related features

--- packages/ag-theme-storybook/stories/2-Features.stories.js
+++ packages/ag-theme-storybook/stories/2-Features.stories.js
@@ -84,3 +84,100 @@ export const HeaderTooltip = () => (
     <AgGridReact columnDefs={columnDefs} rowData={data}></AgGridReact>
   </div>
 );
+
+const columnGroupDefs = [
+  {
+    headerName: "Make and Model",
+    children: [
+      {
+        headerName: "Make",
+        field: "make",
+        headerTooltip: "The Make of the car"
+      },
+      {
+        headerName: "Model",
+        field: "model",
+        headerTooltip: "The model of the car, very long!",
+        columnGroupShow: "closed"
+      }
+    ]
+  },
+  {
+    headerName: "Price",
+    field: "price",
+    headerTooltip: "Price"
+  }
+];
+
+export const ColumnGroups = () => (
+  <div
+    className="ag-theme-alpine"
+    style={{
+      height: "500px",
+      width: "600px"
+    }}
+  >
+    <AgGridReact columnDefs={columnGroupDefs} rowData={data}></AgGridReact>
+  </div>
+);
+
+export const ResizableColumnGroups = () => (
+  <div
+    className="ag-theme-alpine"
+    style={{
+      height: "500px",
+      width: "600px"
+    }}
+  >
+    <AgGridReact
+      defaultColDef={{ resizable: true }}
+      columnDefs={columnGroupDefs}
+      rowData={data}
+    ></AgGridReact>
+  </div>
+);
+
+const pinnedColumnDefs = [
+  {
+    headerName: "Make",
+    field: "make",
+    pinned: "left"
+  },
+  {
+    headerName: "Model",
+    field: "model"
+  },
+  {
+    headerName: "Price",
+    field: "price"
+  },
+  {
+    headerName: "Make",
+    field: "make"
+  },
+  {
+    headerName: "Model",
+    field: "model"
+  },
+  {
+    headerName: "Price",
+    field: "price",
+    pinned: "right"
+  }
+];
+
+export const PinnedColumns = () => (
+  <div
+    className="ag-theme-alpine"
+    style={{
+      height: "500px",
+      width: "700px"
+    }}
+  >
+    <AgGridReact
+      defaultColDef={{ resizable: true }}
+      columnDefs={pinnedColumnDefs}
+      rowData={data}
+    ></AgGridReact>
+  </div>
+);
