c9ec7e859bd2318391180443156e43325235fa7a
Petyo Ivanov <underlog@gmail.com>
2019-09-26 19:15:17 +0300

    Rich Select

---
+++ packages/ag-theme-storybook-enterprise/stories/2-Editing.stories.js
@@ -0,0 +1,123 @@
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/src/styles/ag-theme-alpine/sass/ag-theme-alpine.scss";
+import { AgGridReact } from "ag-grid-react";
+import React from "react";
+
+export default {
+  title: "Editing (rich select)"
+};
+
+const data = [
+  {
+    make: "Toyota",
+    model: "Celica",
+    price: 35000
+  },
+  {
+    make: "Ford",
+    model: "Mondeo",
+    price: 32000
+  },
+  {
+    make: "Porsche",
+    model: "Boxter",
+    price: 72000
+  },
+  {
+    make: "Toyota",
+    model: "Celica",
+    price: 35000
+  },
+  {
+    make: "Ford",
+    model: "Mondeo",
+    price: 32000
+  },
+  {
+    make: "Porsche",
+    model: "Boxter",
+    price: 72000
+  },
+  {
+    make: "Toyota",
+    model: "Celica",
+    price: 35000
+  },
+  {
+    make: "Ford",
+    model: "Mondeo",
+    price: 32000
+  },
+  {
+    make: "Porsche",
+    model: "Boxter",
+    price: 72000
+  }
+];
+
+const columnDefs = [
+  {
+    headerName: "Make (select)",
+    field: "make",
+    editable: true,
+    cellEditor: "agSelectCellEditor",
+    cellEditorParams: {
+      values: ["Toyota", "Ford", "Porsche"]
+    }
+  },
+  {
+    headerName: "Make (rich select)",
+    field: "make",
+    editable: true,
+    cellEditor: "agRichSelectCellEditor",
+    cellEditorParams: {
+      values: ["Toyota", "Ford", "Porsche"]
+    }
+  },
+  {
+    headerName: "Model (text)",
+    field: "model",
+    editable: true
+  },
+  {
+    headerName: "Model (popup text)",
+    field: "model",
+    cellEditor: "agPopupTextCellEditor",
+    editable: true
+  },
+  {
+    headerName: "Model (popup large text)",
+    field: "model",
+    cellEditor: "agLargeTextCellEditor",
+    cellEditorParams: {
+      rows: 3,
+      cols: 26
+    },
+    editable: true
+  },
+  {
+    headerName: "Make (popup select)",
+    field: "make",
+    editable: true,
+    cellEditor: "agPopupSelectCellEditor",
+    cellEditorParams: {
+      values: ["Toyota", "Ford", "Porsche"]
+    }
+  }
+];
+
+export const AllEditorsSideBySide = () => (
+  <div
+    className="ag-theme-alpine"
+    style={{
+      height: "500px",
+      width: "100%"
+    }}
+  >
+    <AgGridReact
+      columnDefs={columnDefs}
+      rowData={data}
+      rowSelection="multiple"
+    ></AgGridReact>
+  </div>
+);
