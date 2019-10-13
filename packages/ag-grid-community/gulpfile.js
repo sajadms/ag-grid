7d644a52fa6e5711e28d3d840266a68358f3c0e7
Petyo Ivanov <underlog@gmail.com>
2019-10-13 09:24:08 +0300

    Build

--- packages/ag-grid-community/gulpfile.js
+++ packages/ag-grid-community/gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const path = require('path');
 const clean = require('gulp-clean');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const gulpTypescript = require('gulp-typescript');
 const typescript = require('typescript');
@@ -16,18 +16,18 @@ const replace = require('gulp-replace');
 
 const bundleTemplate = '// <%= pkg.name %> v<%= pkg.version %>\n';
 
-const headerTemplate = ['/**',
+const headerTemplate = [
+    '/**',
     ' * <%= pkg.name %> - <%= pkg.description %>',
     ' * @version v<%= pkg.version %>',
     ' * @link <%= pkg.homepage %>',
     ' * @license <%= pkg.license %>',
     ' */',
-    ''].join('\n');
+    ''
+].join('\n');
 
 const dtsHeaderTemplate =
-    '// Type definitions for <%= pkg.name %> v<%= pkg.version %>\n' +
-    '// Project: <%= pkg.homepage %>\n' +
-    '// Definitions by: Niall Crosby <https://github.com/ag-grid/>\n';
+    '// Type definitions for <%= pkg.name %> v<%= pkg.version %>\n' + '// Project: <%= pkg.homepage %>\n' + '// Definitions by: Niall Crosby <https://github.com/ag-grid/>\n';
 
 gulp.task('default', ['webpack-all']);
 gulp.task('release', ['webpack-all']);
@@ -67,51 +67,39 @@ function tscWatch() {
 }
 
 function cleanDist() {
-    return gulp
-        .src('dist', {read: false})
-        .pipe(clean());
+    return gulp.src('dist', { read: false }).pipe(clean());
 }
 
 function cleanMain() {
-    return gulp
-        .src(['./main.d.ts', 'main.js'], {read: false})
-        .pipe(clean());
+    return gulp.src(['./main.d.ts', 'main.js'], { read: false }).pipe(clean());
 }
 
 function tscSrcTask() {
-    const tsProject = gulpTypescript.createProject('./tsconfig.json', {typescript: typescript});
+    const tsProject = gulpTypescript.createProject('./tsconfig.json', { typescript: typescript });
 
-    const tsResult = gulp
-        .src('src/ts/**/*.ts')
-        .pipe(tsProject());
+    const tsResult = gulp.src('src/ts/**/*.ts').pipe(tsProject());
 
     return merge([
-        tsResult.dts
-            .pipe(header(dtsHeaderTemplate, {pkg: pkg}))
-            .pipe(gulp.dest('dist/lib')),
-        tsResult.js
-            .pipe(header(headerTemplate, {pkg: pkg}))
-            .pipe(gulp.dest('dist/lib'))
+        tsResult.dts.pipe(header(dtsHeaderTemplate, { pkg: pkg })).pipe(gulp.dest('dist/lib')),
+        tsResult.js.pipe(header(headerTemplate, { pkg: pkg })).pipe(gulp.dest('dist/lib'))
     ]);
 }
 
 function tscMainTask() {
-    const tsProject = gulpTypescript.createProject('./tsconfig-main.json', {typescript: typescript});
+    const tsProject = gulpTypescript.createProject('./tsconfig-main.json', { typescript: typescript });
 
-    const tsResult = gulp
-        .src('./src/ts/main.ts')
-        .pipe(tsProject());
+    const tsResult = gulp.src('./src/ts/main.ts').pipe(tsProject());
 
     return merge([
         tsResult.dts
-            .pipe(replace("\"./", "\"./dist/lib/"))
-            .pipe(header(dtsHeaderTemplate, {pkg: pkg}))
-            .pipe(rename("main.d.ts"))
+            .pipe(replace('"./', '"./dist/lib/'))
+            .pipe(header(dtsHeaderTemplate, { pkg: pkg }))
+            .pipe(rename('main.d.ts'))
             .pipe(gulp.dest('./')),
         tsResult.js
-            .pipe(replace("require(\"./", "require(\"./dist/lib/"))
-            .pipe(header(headerTemplate, {pkg: pkg}))
-            .pipe(rename("main.js"))
+            .pipe(replace('require("./', 'require("./dist/lib/'))
+            .pipe(header(headerTemplate, { pkg: pkg }))
+            .pipe(rename('main.js'))
             .pipe(gulp.dest('./'))
     ]);
 }
@@ -124,109 +112,117 @@ function webpackTask(minify, styles) {
     fileName += styles ? '' : '.noStyle';
     fileName += '.js';
 
-    return gulp.src('src/entry.js')
-        .pipe(webpackStream({
-            mode: minify ? 'production' : 'none',
-            entry: {
-                main: mainFile
-            },
-            output: {
-                path: path.join(__dirname, "dist"),
-                filename: fileName,
-                library: ["agGrid"],
-                libraryTarget: "umd"
-            },
-            //devtool: 'inline-source-map',
-            module: {
-                rules: [
-                    {
-                        test: /\.css$/, 
-                        use: ['style-loader', {
-                            loader:'css-loader',
-                            options: {
-                                minimize: !!minify
-                            }
-                        }]
-                    }
-                ]
-            }
-        }))
-        .pipe(header(bundleTemplate, {pkg: pkg}))
+    return gulp
+        .src('src/entry.js')
+        .pipe(
+            webpackStream({
+                mode: minify ? 'production' : 'none',
+                entry: {
+                    main: mainFile
+                },
+                output: {
+                    path: path.join(__dirname, 'dist'),
+                    filename: fileName,
+                    library: ['agGrid'],
+                    libraryTarget: 'umd'
+                },
+                //devtool: 'inline-source-map',
+                module: {
+                    rules: [
+                        {
+                            test: /\.css$/,
+                            use: [
+                                'style-loader',
+                                {
+                                    loader: 'css-loader',
+                                    options: {
+                                        minimize: !!minify
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            })
+        )
+        .pipe(header(bundleTemplate, { pkg: pkg }))
         .pipe(gulp.dest('./dist/'));
 }
 
 function scssTask() {
-    var f = filter(["**/*.css", '!*Font*.css'], { restore: true });
+    var f = filter(['**/*.css', '!*Font*.css'], { restore: true });
 
-    return gulp.src(['src/styles/**/*.scss', '!src/styles/**/_*.scss'])
+    return gulp
+        .src(['src/styles/**/*.scss', '!src/styles/**/_*.scss', '!**/node_modules/**/*.scss'])
         .pipe(named())
-        .pipe(webpackStream({
-            mode: 'none',
-            module: {
-                rules: [
-                    {
-                        test: /\.scss$/,
-                        use: [
-                            MiniCssExtractPlugin.loader,
-                            {
-                                loader: "css-loader",
-                                options: {
-                                    minimize: false
-                                }
-                            },
-                            "sass-loader",
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    syntax: 'postcss-scss', 
-                                    plugins: [autoprefixer({
-                                        overrideBrowserslist: ["last 2 version"],
-                                        flexbox: true
-                                    })]
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        test: /\.(svg)$/,
-                        use: [
-                            'cache-loader',
-                            {
-                                loader: 'url-loader',
-                                options: {
-                                    limit: 8192
-                                }
-                            },
-                            {
-                                loader: 'image-webpack-loader',
-                                options: {
-                                    svgo: {
-                                        cleanupAttrs: true,
-                                        removeDoctype: true,
-                                        removeComments: true,
-                                        removeMetadata: true,
-                                        removeTitle: true,
-                                        removeDesc: true,
-                                        removeEditorsNSData: true,
-                                        removeUselessStrokeAndFill: true,
-                                        cleanupIDs: true,
-                                        collapseGroups: true,
-                                        convertShapeToPath: true
+        .pipe(
+            webpackStream({
+                mode: 'none',
+                module: {
+                    rules: [
+                        {
+                            test: /\.scss$/,
+                            use: [
+                                MiniCssExtractPlugin.loader,
+                                {
+                                    loader: 'css-loader',
+                                    options: {
+                                        minimize: false
+                                    }
+                                },
+                                'sass-loader',
+                                {
+                                    loader: 'postcss-loader',
+                                    options: {
+                                        syntax: 'postcss-scss',
+                                        plugins: [
+                                            autoprefixer({
+                                                overrideBrowserslist: ['last 2 version'],
+                                                flexbox: true
+                                            })
+                                        ]
                                     }
                                 }
-                            }
-                        ]
-                    }
-                ]
-            },
-            plugins: [
-                new MiniCssExtractPlugin('[name].css')
-            ]
-        }))
+                            ]
+                        },
+                        {
+                            test: /\.(svg)$/,
+                            use: [
+                                'cache-loader',
+                                {
+                                    loader: 'url-loader',
+                                    options: {
+                                        limit: 8192
+                                    }
+                                },
+                                {
+                                    loader: 'image-webpack-loader',
+                                    options: {
+                                        svgo: {
+                                            cleanupAttrs: true,
+                                            removeDoctype: true,
+                                            removeComments: true,
+                                            removeMetadata: true,
+                                            removeTitle: true,
+                                            removeDesc: true,
+                                            removeEditorsNSData: true,
+                                            removeUselessStrokeAndFill: true,
+                                            cleanupIDs: true,
+                                            collapseGroups: true,
+                                            convertShapeToPath: true
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                plugins: [new MiniCssExtractPlugin('[name].css')]
+            })
+        )
         .pipe(f)
         .pipe(gulp.dest('dist/styles/'))
         .pipe(f.restore)
         .pipe(filter('*Font*.css'))
         .pipe(gulp.dest('dist/styles/webfont/'));
 }
-
